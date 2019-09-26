d6cf37be6c7dc9a1a8758a26067530e8dbbe41b2
Yevhenii Baranov <yevhenii.baranov@teamdev.com>
2019-09-27 00:30:41 +0300

    Add code for reading arguments for an agent.

--- src/main/java/ua/nure/baranov/quickstart/BookBuyer.java
+++ src/main/java/ua/nure/baranov/quickstart/BookBuyer.java
@@ -1,11 +1,32 @@
 package ua.nure.baranov.quickstart;
 
+import jade.core.AID;
 import jade.core.Agent;
 
 public class BookBuyer extends Agent {
 
+    private String targetBookTitle;
+
+    private AID[] sellerAgents = {
+            new AID("seller1", AID.ISLOCALNAME),
+            new AID("seller2", AID.ISLOCALNAME)
+    };
+
     @Override
     protected void setup() {
         System.out.println("Hello there! My name is " + getAID().getName());
+        Object[] arguments = getArguments();
+        if (arguments != null && arguments.length != 0) {
+            targetBookTitle = (String) arguments[0];
+            System.out.println("I'll try to buy " + targetBookTitle);
+        } else {
+            System.out.println("Don't have anything to buy. Bye!");
+            doDelete();
+        }
+    }
+
+    @Override
+    protected void takeDown() {
+        System.out.println("Agent " + getAID().getName() + " is shutting down");
     }
 }
