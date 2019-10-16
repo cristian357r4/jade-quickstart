e31cfd9d4c5451e4f18c691e2587a55b5511e3bd
Yevhenii Baranov <yevhenii.baranov@teamdev.com>
2019-10-16 23:00:56 +0300

    Connect buyer and seller through the "yellow pages".

--- src/main/java/ua/nure/baranov/quickstart/BookBuyer.java
+++ src/main/java/ua/nure/baranov/quickstart/BookBuyer.java
@@ -3,7 +3,13 @@ package ua.nure.baranov.quickstart;
 import jade.core.AID;
 import jade.core.Agent;
 import jade.core.behaviours.TickerBehaviour;
+import jade.domain.DFService;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.domain.FIPAException;
 import ua.nure.baranov.quickstart.behaviour.RequestPerformer;
+
+import java.util.Arrays;
 
 public class BookBuyer extends Agent {
 
@@ -21,6 +27,27 @@ public class BookBuyer extends Agent {
         if (arguments != null && arguments.length != 0) {
             targetBookTitle = (String) arguments[0];
             System.out.println("I'll try to buy " + targetBookTitle);
+
+            addBehaviour(new TickerBehaviour(this, 60000) {
+                @Override
+                protected void onTick() {
+                    final DFAgentDescription templateDescription = new DFAgentDescription();
+                    final ServiceDescription serviceDescription = new ServiceDescription();
+
+                    serviceDescription.setType("book-selling");
+                    templateDescription.addServices(serviceDescription);
+
+                    try {
+                        final DFAgentDescription[] result = DFService.search(myAgent, templateDescription);
+                        sellerAgents = Arrays.stream(result)
+                                .map(DFAgentDescription::getName)
+                                .toArray(AID[]::new);
+
+                    } catch (FIPAException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
 
             addBehaviour(new TickerBehaviour(this, 60000) {
                 @Override
--- src/main/java/ua/nure/baranov/quickstart/BookSeller.java
+++ src/main/java/ua/nure/baranov/quickstart/BookSeller.java
@@ -2,6 +2,10 @@ package ua.nure.baranov.quickstart;
 
 import jade.core.Agent;
 import jade.core.behaviours.OneShotBehaviour;
+import jade.domain.DFService;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.domain.FIPAException;
 import ua.nure.baranov.quickstart.behaviour.OffsetRequestsServer;
 import ua.nure.baranov.quickstart.behaviour.PurchaseOrdersServer;
 import ua.nure.baranov.quickstart.ui.mock.BookSellerGui;
@@ -23,10 +27,30 @@ public class BookSeller extends Agent {
 
         addBehaviour(new OffsetRequestsServer(catalogue));
         addBehaviour(new PurchaseOrdersServer());
+
+        // Register the agent!
+
+        final DFAgentDescription description = new DFAgentDescription();
+        description.setName(this.getAID());
+        final ServiceDescription serviceDescription = new ServiceDescription();
+        serviceDescription.setType("book-selling");
+        serviceDescription.setName("JADE-quickstart-book-seller");
+        description.addServices(serviceDescription);
+
+        try {
+            DFService.register(this, description);
+        } catch (FIPAException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     protected void takeDown() {
+        try {
+            DFService.deregister(this);
+        } catch (FIPAException e) {
+            e.printStackTrace();
+        }
         gui.dispose();
         System.out.println("Book seller, ID " + this.getAID().getName() + ", going down");
     }
